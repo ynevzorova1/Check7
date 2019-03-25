565a758a803d36726194cfc2427a4c5946e4773f
Yulia Nevzorova <ynevzorova@gmail.com>
2019-03-25 16:12:57 +0200

    Add project files.

---
+++ Check7/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Check7")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Check7")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ac51389c-22c1-497b-874f-486221ffd7bb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Check7/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Check7
+{
+
+    public class UnitTest1
+    {
+
+        IWebDriver driver;
+
+        [Test]
+
+        public void Test()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://old.qalight.com.ua/zapisatsya-na-kursy");
+
+           ICollection<IWebElement> count = driver.FindElements(By.XPath("//div[@id='footer_social']/ul/li"));
+
+            Assert.AreEqual(count.Count, 7,"Элементов не 7");
+        }
+
+
+    }
+}
